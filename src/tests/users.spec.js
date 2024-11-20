2046a3e22b0187ca9d0cc92354df59241d7b9cf0
foly13 <foly_13@hotmail.com>
2024-11-20 13:11:56 -0300

    retoques

--- src/tests/users.spec.js
+++ src/tests/users.spec.js
@@ -5,29 +5,32 @@ const testUsersFilePath = path.join(__dirname, 'dataTemporal/testUsers.json');
 
 // Mock de controlador y middleware
 jest.mock('../controllers/usersController', () => ({
-  getAllUsers: jest.fn((req, res) => res.status(200).json([{ id: 1, name: 'User 1' }, { id: 2, name: 'User 2' }])),
+  getAllUsers: jest.fn((req, res) => {
+    const testUsers = require('./dataTemporal/testUsers.json');
+    res.status(200).json(testUsers);
+  }),
   renderNewUserForm: jest.fn((req, res) => res.status(200).send('Render form')),
   addNewUser: jest.fn((req, res) => res.status(201).json({ id: 3, name: 'New User' })),
   renderEditUserForm: jest.fn((req, res) => res.status(200).send('Edit user form')),
   updateUser: jest.fn((req, res) => res.status(200).json({ id: req.params.id, name: 'Updated User' })),
-  deleteUser: jest.fn((req, res) => res.status(200).json({ message: 'User deleted' })) // Mock de deleteUser
+  deleteUser: jest.fn((req, res) => res.status(200).json({ message: 'User deleted' })) 
 }));
 
 jest.mock('../middlewares/adminMiddleware', () => jest.fn((req, res, next) => next()));
 
+//tests
+
 describe('GET /users', () => {
-  it('should return a list of users', async () => {
+  test('should return a list of users from the JSON file', async () => {
     const response = await request(server).get('/users');
     expect(response.statusCode).toBe(200);
-    expect(response.body).toEqual([
-      { id: 1, name: 'User 1' },
-      { id: 2, name: 'User 2' },
-    ]);
+    const testUsers = require('./dataTemporal/testUsers.json');
+    expect(response.body).toEqual(testUsers);
   });
 });
 
 describe('POST /users', () => {
-  it('should add a new user', async () => {
+  test('should add a new user', async () => {
     const newUser = { name: 'New User' };
     const response = await request(server).post('/users').send(newUser);
     expect(response.statusCode).toBe(201);
@@ -36,7 +39,7 @@ describe('POST /users', () => {
 });
 
 describe('GET /users/:id/edit', () => {
-  it('should render the edit user form', async () => {
+  test('should render the edit user form', async () => {
     const response = await request(server).get('/users/1/edit');
     expect(response.statusCode).toBe(200);
     expect(response.text).toBe('Edit user form');
@@ -44,7 +47,7 @@ describe('GET /users/:id/edit', () => {
 });
 
 describe('DELETE /users/:id', () => {
-  it('should delete a user', async () => {
+  test('should delete a user', async () => {
     const response = await request(server).delete('/users/1');
     expect(response.statusCode).toBe(200);
     expect(response.body).toEqual({ message: 'User deleted' });
